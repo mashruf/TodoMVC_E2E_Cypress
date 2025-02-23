aa480643ce58b6b77c421c54effe86777d30876e
Mashruf Rohon <mashrufrohon@gmail.com>
2025-02-23 20:43:44 +0600

    Delete todo

--- cypress/e2e/DeleteTodo.cy.js
+++ cypress/e2e/DeleteTodo.cy.js
@@ -1,16 +1,14 @@
 /// <reference types="cypress" />
 
-describe("Delete todo",()=>{
+describe("Clear complete todo",()=>{
     before("",()=>{
         cy.visitWebsite();
     })
 
-    it("Should delete a todo",()=>{
+    it("Should clear a complete todo",()=>{
         cy.get(".new-todo").type("New todo{enter}");
         cy.get(".view").should("contain","New todo");
-        cy.get(".view").find(".toggle").should("be.exist").click();
-        cy.get(".todo-count").should("contain","0 items left!");
-        cy.get(".clear-completed").click();
+        cy.get(".view").find(".destroy").should("be.exist").click({force:true});
         cy.get(".todo-count").should("not.be.exist");
     })
 })
