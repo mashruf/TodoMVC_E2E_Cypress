09bc10f1fc12bad9f50c66f2cc2021b007cf4143
Mashruf Rohon <mashrufrohon@gmail.com>
2025-05-03 01:27:41 +0600

    modified cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // Required if you're using reporters or plugins
+      require('cypress-mochawesome-reporter/plugin')(on);
+
     }
   }
 })
