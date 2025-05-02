ce052c966df31bd905148b3ceebce58c6d37232a
Mashruf Rohon <mashrufrohon@gmail.com>
2025-05-03 00:01:59 +0600

    fixed specs

--- cypress/e2e/AddNewTodo.cy.js
+++ cypress/e2e/AddNewTodo.cy.js
@@ -5,7 +5,7 @@ describe("Add todo",()=>{
     it("Should add a new todo",()=>{
         cy.visitWebsite();
         cy.get(".new-todo").type("New todo{enter}");
-        cy.get(".view").should("contain","New todo77");
+        cy.get(".view").should("contain","New todo");
     })
 
 })
