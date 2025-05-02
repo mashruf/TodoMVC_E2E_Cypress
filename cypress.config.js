ed3799e51d6a28c33b760513b9146deb5b611001
Mashruf Rohon <mashrufrohon@gmail.com>
2025-05-02 23:15:50 +0600

    config.js file

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,16 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  video: true,
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      require('cypress-mochawesome-reporter/plugin')(on);
+      // Required if you're using reporters or plugins
     },
-  },
-});
+    screenshotOnRunFailure: true,
+    video: true,
+    videoUploadOnPasses: false, // optional: keep videos even if tests pass
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
+  }
+})
 
-//
